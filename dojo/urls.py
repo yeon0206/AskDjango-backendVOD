9c62d8b780587e61e4445bd3f32ef090d7739a7f
GIYEON KWON <brucek0206@gmail.com>
2017-11-03 00:51:43 +0900

    dofo.views.mysum에서 다수 숫자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -2,8 +2,15 @@
 from django.conf.urls import url
 from . import views
 
+'''
 urlpatterns =[
     url(r'^sum/(?P<x>\d+)/$', views.mysum), #숫자를 하나만 받기
     url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum), #숫자를 두개 받기
     url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum), #숫자를 세개 받기
+]
+'''
+
+urlpatterns =[
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+        #[\d/]+ : 숫자혹은 /가 하나이상 반복이된다면
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -2,7 +2,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+'''
 def mysum(request,x,y=0,z=0): #y=0,z=0으로 default 값을 지정해준다.
 #view함수는 클라이언트가 서버로 넘길때, 첫번째 인자는 무조건 request를 받는다.
 #request: HttpRequest 는 request의 인스턴스
     return HttpResponse(int(x)+int(y)+int(z))
+'''
+
+def mysum(request,numbers):
+    # numbers = "1/2/12/123/12312/312/123123"
+    # result=sum(map(int, numbers.split('/')))  
+    # /슬러쉬가 하나이상 들어가도 실행이 됨> 그런데 // 사이에 빈문자열이 나옴 > int로 변환이 안됨 > ValueError 발생
+
+    result=sum(map(lambda s: int(s or 0), numbers.split('/'))) # or는 s가 거짓일때() 0으로 치환되서 처리
+    return HttpResponse(result)
