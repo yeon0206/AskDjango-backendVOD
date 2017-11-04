95f08f0f3046c54af29bc3f900084742ccc1105d
GIYEON KWON <brucek0206@gmail.com>
2017-11-04 15:05:36 +0900

    blog.views.post_list 뷰에 QuerySet 검색 구현

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,12 @@ from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
+    q = request.GET.get('q','') #request get 쿼리셋어 q가있으면 가져오고, 없으면 빈문자열
+    if q:
+        qs = qs.filter(title__icontains=q)
+
     ctx={
         'post_list': qs,
+        'q' : q,
     }
     return render(request, 'blog/post_list.html',ctx)
