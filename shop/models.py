a25d811ca29c8a9b4ac94b5af3543be75d661972
GIYEON KWON <brucek0206@gmail.com>
2017-11-06 01:57:03 +0900

    related_name 예시로서, shop.models.Post모델생성

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+')
+    # # blog/models.py
+    # class Post(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blog_post_set')
+    # # shop/models.py
+    # class Post(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='shop_post_set') 
+     # +: user.post_set.all() 사용을 포기하겠다.
+     # shop.models.Post.objects.filter(user=user)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    upadated_at = models.DateTimeField(auto_now=True)
