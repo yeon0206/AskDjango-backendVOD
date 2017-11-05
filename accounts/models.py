46b875db2bb216a6a6ae9e21b8d946bfcca98c90
GIYEON KWON <brucek0206@gmail.com>
2017-11-06 01:21:48 +0900

    settings.AUTO_USER_MODEL의 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 #accounts/models.py
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User) # BAD CASE!
+    # user = models.OneToOneField(User) # BAD CASE! 다른유저모델을 쓸수 있기때문에
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
