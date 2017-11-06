660f6605374496c54692827a7f4128d620512737
GIYEON KWON <brucek0206@gmail.com>
2017-11-06 17:53:21 +0900

    프로젝트 템플릿에 bootstrap3스타일 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -14,3 +14,10 @@ class PostCreateView(CreateView):
     # success_url = '/'
 
 post_new = PostCreateView.as_view()
+
+'''
+get_absolute_url 은 URL Reverse 를 수행하는 함수에 의해서 호출됩니다.
+
+1. django.shortcuts.resolve_url 함수를 쓸 때
+2. django.views.generic.CreateView, django.views.generic.UpdateView 등의 CBV를 쓸 때, success_url 로서 사용됩니다.
+'''
