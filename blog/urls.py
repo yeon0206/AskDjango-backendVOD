81bc6ae74b4201613909d604a8b9203b2f0aa3e2
GIYEON KWON <brucek0206@gmail.com>
2017-11-07 19:40:43 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), # 포스팅 목록
     # url(r'^new/$', views.post_new, name='post_new'), # 새 포스팅
     url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'), # 포스팅 보기(post_detail일 함수는 id라는 이름의 필드를 받을수 있어야한다)
+
+    url(r'^new/$', views.post_new, name="post_new"),
+    url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^cbv/new/$', views_cbv.post_new), # 포스팅 목록
     # url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'), # 포스팅 수정
     # url(r'^(?P<id>\d+)/delete/$', views.post_delete, name='post_delete'), # 포스팅 삭제
