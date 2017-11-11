8103c48d9451f8107a16a4e185a1b829d90f9119
GIYEON KWON <brucek0206@gmail.com>
2017-11-12 00:56:15 +0900

    회원가입 시에 휴대폰번호/주소 추가 입력

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,23 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+class SignupForm(UserCreationForm): #SignupForm은 User모델에 대한 모델폼, 모델폼은 하나의 모델만 지원, SignupForm->User모델
+    phone_number = forms.CharField()
+    address = forms.CharField()
 
-class SignupForm(UserCreationForm):
+    #폰넘버,어드레스는 Profile모델의 필드. 따라서, User모델 인스턴스 생성 시 Profile모델 인스턴스도 같이 생성해줌.
+    #SignupForm은 User모델에 대한 폼이기 때문에, 단순히 phone_number 필드만 정의해주면 지원해주지 않고 저장되지 않는다.
+    #따라서 따로 저장해줘야함
     class Meta(UserCreationForm.Meta): #UserCreationForm에 기존 Meta를 새로 정의 했기때문에, 상속으로 받아주고, 원하는 값 fields 만 재정의하겠다.
         # fields = ('username', 'email')
         fields = UserCreationForm.Meta.fields + ('email',)
         #UserCreationForm이 User모델에 대한 필드이며 User모델은 email필드를 가져있기때문에 호출가능
+    def save(self):
+        user = super().save() #SignupForm에 save를 호출했으니 관련모델인 user인스턴스를 리턴
 
-        
+        profile = Profile.objects.create(
+            user = user,
+            phone_number = self.cleaned_data['phone_number'],
+            address = self.cleaned_data['address'],
+        )
+        return user
